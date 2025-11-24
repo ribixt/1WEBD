16524ccaa484c61fdd2d36b23257e833cf664ce2
Robin H <robinhardy14@gmail.com>
2025-11-24 16:34:51 +0100

    Day1

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("Hello World!");
+
+// Exercice 1
+/*
+let HT = prompt("Saisissez un prix HT: ");
+let TVA = prompt("Saisissez un taux de TVA: ");
+let article = prompt("Saisissez un nombre d'article: ");
+const calculPrix = (HT * article) * TVA;
+console.log(calculPrix + "€");
+*/
+
+// Exercic 2
+/*
+let a = 5;
+let b = 15;
+let c = a;
+a = b;
+b = c;
+console.log("a =",a,"b = ",b)
+*/
+
+/*
+let a = 13;
+let b = 50;
+[a,b] = [b,a];
+console.log("a =",a,"b =",b)
+*/
+
+/*
+let a = 23;
+let b = 86;
+a = a + b
+b = a - b
+a = a - b
+console.log("a =",a,"b =",b)
+*/
+
+// Exercic 3
+/*
+let x = 2;
+let y = 4;
+let z = 6;
+let temp = x;
+
+x = y
+y = z
+z = temp
+
+console.log("x =",x,"y =",y,"z= ",z)
+*/
